9aba235b78575bef0322294ff0a65fffe8083855
Quentin P <pre.quentin@gmail.com>
2014-03-24 21:37:13 +0100

    [tst]

--- engine/AEngine-core.js
+++ engine/AEngine-core.js
@@ -11,8 +11,20 @@
 (function() {
   var AEController, AEEvent, AEGamePhase, AEGamePhaseManager, AEIdFactory, AEMessageBox, AEModel, AEObject, AEPhaseStatusEnum, AESingleton, AEView, AEWorker, AEngine,
     __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
+    __extends = function(child, parent) { 
+      for (var key in parent) {
+        if (__hasProp.call(parent, key))
+          child[key] = parent[key];
+      }
+      function ctor() {
+        this.constructor = child; 
+      } 
+      ctor.prototype = parent.prototype; 
+      child.prototype = new ctor(); 
+      child.__super__ = parent.prototype; 
+      return child; 
+    };
+    
   AEWorker = (function(_super) {
 
     __extends(AEWorker, _super);
